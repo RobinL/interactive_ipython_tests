212ca800baeef01d4921691e05be0ce2b9f2fcdb
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2016-10-22 16:08:15 +0100

    Now with flexbox interface into D3

--- d3_test.py
+++ d3_test.py
@@ -1,31 +1,49 @@
 from IPython.display import Javascript, display, clear_output
-from ipywidgets import IntSlider
+from ipywidgets import IntSlider, FloatSlider, Layout, HBox
 import math
 from functools import partial
+
 
 
 
 class Interface:
 
     def __init__(self):
+        self.widgets = {}
         self.display_interface()
 
-    def get_d3_script(self, template,square_size, num_squares=10,step=2):
+    def get_d3_script(self, template):
+
+        num_squares = self.widgets["num_squares_widget"].value
+        square_size = self.widgets["square_size_widget"].value
+        step = self.widgets["step_widget"].value
         squares = range(num_squares)
         squares = [math.sin(i/step)*square_size for i in squares]    
+
         return template.format(squares=squares)
 
-    def display_d3(self, passed_arguments):
+    def display_d3(self,caller):
         js_template = open("custom_js.js").read()
-        script = self.get_d3_script(js_template, passed_arguments["owner"].value)
+        script = self.get_d3_script(js_template)
         clear_output(True)
         display(Javascript(script))
+    
 
+
+
+# http://ipywidgets.readthedocs.io/en/latest/examples/Widget%20List.html
     def display_interface(self):
-        display(Javascript("""$.getScript('http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js')"""))
-        self.int_widget = IntSlider()
+        Javascript("""$.getScript('d3.min.js')""")
+        self.widgets["square_size_widget"] = FloatSlider(description="Choose square size: ", value = 20, min = 5, max=40)
+        self.widgets["num_squares_widget"] = IntSlider(description="Choose num squares: ", value = 20, min = 1, max=100)
+        self.widgets["step_widget"] = FloatSlider(description="Choose step ", value = 3, min = 1, max=10)
+
+        widget_list = []
+        for k,v in self.widgets.iteritems():
+            v.layout = Layout(flex='1 1 auto', width='auto')
+            widget_list.append(v)
+
+            v.observe(self.display_d3)
+ 
+        display(HBox(widget_list))
         
-     
-        self.int_widget.observe(self.display_d3)
-        display(self.int_widget)
-        # print "hello"
