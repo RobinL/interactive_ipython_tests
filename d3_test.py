b369cef0f870d63c56abad290958c190a2e4f363
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2016-10-22 11:56:36 +0100

    d3 now works factored out

--- d3_test.py
+++ d3_test.py
@@ -1,18 +1,31 @@
-from IPython.display import Javascript, display
+from IPython.display import Javascript, display, clear_output
 from ipywidgets import IntSlider
-
-def get_d3_script(template,square_size, num_squares,step):
-    squares = range(num_squares)
-    squares = [math.sin(i/step)*square_size for i in squares]    
-    return template.format(squares=squares)
+import math
+from functools import partial
 
 
 
+class Interface:
 
-def display_d3():
-    Javascript("""$.getScript('http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js')""")
-    js_template = open("custom_js.js").read()
-	w = IntSlider()
-    script = get_d3_script(js_template)
-	display(w)
-    
+    def __init__(self):
+        self.display_interface()
+
+    def get_d3_script(self, template,square_size, num_squares=10,step=2):
+        squares = range(num_squares)
+        squares = [math.sin(i/step)*square_size for i in squares]    
+        return template.format(squares=squares)
+
+    def display_d3(self, passed_arguments):
+        js_template = open("custom_js.js").read()
+        script = self.get_d3_script(js_template, passed_arguments["owner"].value)
+        clear_output(True)
+        display(Javascript(script))
+
+    def display_interface(self):
+        display(Javascript("""$.getScript('http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js')"""))
+        self.int_widget = IntSlider()
+        
+     
+        self.int_widget.observe(self.display_d3)
+        display(self.int_widget)
+        # print "hello"
