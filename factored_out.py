dce67d8fecf586db30e050dfab80a904823ce452
Robin Linacre <robinlinacre@hotmail.com>
2016-10-22 09:35:35 +0100

    Initial commit - basic display_interface function works

---
+++ factored_out.py
@@ -0,0 +1,17 @@
+from ipywidgets import widgets
+from IPython.display import display
+from IPython.display import HTML
+
+global_vars = {}
+
+def handle_submit(sender):
+    print(global_vars["text_widget"].value)
+    global_vars["text_widget"].value = global_vars["text_widget"].value.upper()
+
+def display_interface():
+    display(HTML(open("custom_styles.html").read()))
+    global_vars["text_widget"] =  widgets.Text()
+    global_vars["text_widget"].on_submit(handle_submit)
+    display(global_vars["text_widget"])
+
+display_interface()
